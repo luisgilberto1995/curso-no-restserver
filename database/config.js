bd3e439d8a7b0b5cd7398e099e767aa8ed9ca632
Luis Barrios <luisgil95crash@gmail.com>
2021-06-29 18:22:29 -0600

    modificando cadena de conexion

--- database/config.js
+++ database/config.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const dbConnection = async () => {
     try {
-        await mongoose.connect( process.env.MONGODB_ATLAS, {
+        await mongoose.connect( process.env.MONGODB_CNN, {
             useNewUrlParser: true,
             useUnifiedTopology: true,
             useCreateIndex: true,
